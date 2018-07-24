f1b257d0bc039606d1728491ae0a889bd02a023d
PaulinaCoding <paulina.pietrykiewicz@gmail.com>
2018-07-24 23:37:15 +0000

    Debugged rspec for loggin with repeated email and completed rspec for email with minimium characters length

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,8 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
-  validates :password, presence: true,length: {minimum: 3}
+  validates :password, presence: true, length: {minimum: 3}
   validates :password_confirmation, presence: true
 
 end
--- spec/models/user_spec_spec.rb
+++ spec/models/user_spec_spec.rb
@@ -4,68 +4,50 @@ RSpec.describe User, type: :model do
   describe 'Validations' do
 
 
-    it "is valid with all valid attributes" do
+    it "should have all valid attributes" do
       @user = User.create first_name: "Tyrion", last_name: "Lannister", email: "tyrion@tyrion.com", password: "abc", password_confirmation: "abc"
-     expect(@user).to be_valid
+      expect(@user).to be_valid
     end
       
 
-    it "is not valid without a first name" do
+    it "should not be valid without a first name" do
         @user = User.create last_name: "Lannister", email: "tyrion@tyrion.com", password: "abc", password_confirmation: "abc"
         expect(@user.errors.full_messages).to include("First name can't be blank")
     end
 
-    it "is not valid without a last name" do
+    it "should not be valid without a last name" do
       @user = User.create first_name: "Tyrion", email: "tyrion@tyrion.com", password: "abc", password_confirmation: "abc"
       expect(@user.errors.full_messages).to include("Last name can't be blank")
     end
 
-    it "is not valid without a password" do
+    it "should not be valid without a password" do
       @user = User.create first_name: "Tyrion", last_name: "Lannister", email: "tyrion@tyrion.com", password_confirmation: "abc"
       expect(@user.errors.full_messages).to include("Password can't be blank")
     end
 
-    it "is not valid without password confirmation" do
+    it "should not be valid without password confirmation" do
       @user = User.create first_name: "Tyrion", last_name: "Lannister", email: "tyrion@tyrion.com", password: "abc"
       expect(@user.errors.full_messages).to include("Password confirmation can't be blank")
     end
 
 
-    describe 'log-in' do
-      before :each do
-        # create a success case
-        # correct input parameters
-        @user_credentials = {email: 'john.doe@gmail.com', password: 'abc'}
-        # check if existing email in db matches the input email parameter 
-        # make sure you lowercase the email from db as well as the input email parameter
-        @user = User.where('lower(email) like ?', @user_credentials[:email])[0]
-      end
+    it 'email must be unique but not case sensitive' do
+      @user = User.create first_name: "Tyrion", last_name: "Lannister", email: "tyrion@tyrion.com", password: "abc", password_confirmation: "abc"
+      @second_user = User.create first_name: "Kate", last_name: "Smith", email: "TyRioN@tyrioN.COM", password: "345", password_confirmation: "345"
+      # expect(@second_user).to_not be_valid 
+      # puts @second_user.errors.full_messages
+      expect(@second_user.errors.full_messages).to include("Email has already been taken")
+    end 
 
-      it 'email must be unique but not case sensitive' do
-        @user_credentials[:email] = 'JohN.DoE@gmail.com'
-        @user = User.where('lower(email) like ?', @user_credentials[:email].downcase)[0]
-       expect(@user).to_not be nil
 
-      end 
+    it "password must be minimum 3 characters long." do
+      @user = User.create first_name: "Tyrion", last_name: "Lannister", email: "tyrion@tyrion.com", password: "ab", password_confirmation: "ab"
+      # expect(@user).to_not be_valid 
+      # puts @user.errors.full_messages
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 3 characters)")
     end
 
-   
-    describe "register" do
-      puts "------Testing user password length!---------"
-      # puts  @user.password.length
-      #   before :each do
-        it "password must be minimum 3 characters long." do
-          @user = User.new(User.create first_name: "Tyrion", last_name: "Lannister", email: "tyrion@tyrion.com", password: "ab", password_confirmation: "ab")
-          # expect(@user) to_not be_valid 
-          expect (@user.password).count <= 3
-    # expect @user[:password].length <= 3 
-    #       # have_at_least(8).items
-    # it {is_expected.to validate_length_of(:password).with_minimum(3).with_message("Password should be minimum 3 characters long.")}
-
-        end
-      end
-    end
-
+    
   end
 
   # describe '.authenticate_with_credentials' do
